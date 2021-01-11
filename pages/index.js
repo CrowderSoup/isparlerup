1013ab51c68bb50ab82de4c14a64f7f167389c60
Aaron Crowder <aaron.crowder@gmail.com>
2021-01-11 10:49:49 -0700

    Add stats

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,22 @@ export default function Home() {
       <Head>
         <title>Is Parler Up?</title>
         <link rel="icon" href="/favicon.ico" />
+
+        <!-- Matomo -->
+        <script type="text/javascript">
+          var _paq = window._paq = window._paq || [];
+          /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+          _paq.push(['trackPageView']);
+          _paq.push(['enableLinkTracking']);
+          (function() {
+            var u="//cloud.bsdrocker.com/stats/";
+            _paq.push(['setTrackerUrl', u+'matomo.php']);
+            _paq.push(['setSiteId', '3']);
+            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+            g.type='text/javascript'; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+          })();
+        </script>
+        <!-- End Matomo Code -->
       </Head>
 
       <main className={styles.main}>
