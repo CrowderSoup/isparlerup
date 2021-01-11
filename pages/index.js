346f7173e3452d8471a641a2ed8f31095274bdaf
Aaron Crowder <aaron.crowder@gmail.com>
2021-01-11 11:05:16 -0700

    Move to js file

--- pages/index.js
+++ pages/index.js
@@ -7,20 +7,7 @@ export default function Home() {
       <Head>
         <title>Is Parler Up?</title>
         <link rel="icon" href="/favicon.ico" />
-
-        <script type="text/javascript">
-          var _paq = window._paq = window._paq || [];
-          /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-          _paq.push(['trackPageView']);
-          _paq.push(['enableLinkTracking']);
-          (function() {
-            var u="//cloud.bsdrocker.com/stats/";
-            _paq.push(['setTrackerUrl', u+'matomo.php']);
-            _paq.push(['setSiteId', '3']);
-            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-            g.type='text/javascript'; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-          })();
-        </script>
+        <script type="text/javascript" src="/matomo.js"></script>
       </Head>
 
       <main className={styles.main}>
